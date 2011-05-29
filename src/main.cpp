1f794f85a65202d01cb23d8319d49da93b77f2f4
Michael Smith <msmith@msmith.id.au>
2011-05-30 03:41:22 +1000

    Combinator progress.

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,10 @@
 #include <string>
 #include <stdio.h>
 #include <stdlib.h>
-#include <assert.h>
 
-struct FileAssertRaiser
+struct AssertRaiser
 {
-	FileAssertRaiser(bool condition, char const* file, int line, char const* msg)
+	AssertRaiser(bool condition, char const* file, int line, char const* msg)
 	{
 		if (!condition)
 		{
@@ -20,7 +19,7 @@ struct FileAssertRaiser
 #define STRINGIZE(x) STRINGIZE_DETAIL(x)
 #define ASSERT_OBJ_NAME_DETAIL(a, b) a##b
 #define ASSERT_OBJ_NAME(a, b) ASSERT_OBJ_NAME_DETAIL(a, b)
-#define ASSERT(x) namespace {FileAssertRaiser ASSERT_OBJ_NAME(assertObj, __LINE__)((x), __FILE__, __LINE__, #x);}
+#define ASSERT(x) AssertRaiser ASSERT_OBJ_NAME(assertObj, __LINE__)((x), __FILE__, __LINE__, #x);
 
 struct Cell
 {
@@ -73,6 +72,11 @@ bool operator==(Value const& l, Value const& r)
 	return l.cell == r.cell;
 }
 
+bool operator!=(Value const& l, Value const& r)
+{
+	return l.cell != r.cell;
+}
+
 typedef Value (* Code)(Value context, void* data, Value argument);
 
 template <typename T0, typename T1> struct Apply
@@ -87,52 +91,6 @@ template <typename T0, typename T1> struct Apply
 	Function function;
 	Argument argument;
 };
-
-Value evaluate(Value context, Value v)
-{
-	return v;
-}
-
-template <typename T0, typename T1> Value evaluate(
-		Value context, Apply<T0, T1> const& application)
-{
-	return apply(context, application.function, application.argument);
-}
-
-template <typename T0, typename T1> Apply<T0, T1>::operator Value() const
-{
-	return evaluate(Value(0), *this);
-}
-
-template <typename T0, typename T1>
-Apply<T0, T1> make_apply(T0 const& x0, T1 const& x1)
-{
-	return Apply<T0, T1>(x0, x1);
-}
-
-Apply<Value, Value> operator*(Value const& x0, Value const& x1)
-{
-	return make_apply(x0, x1);
-}
-
-template <typename T00, typename T01, typename T10, typename T11>
-Apply<Apply<T00, T01>, Apply<T10, T11> > operator*(
-		Apply<T00, T01> const& x0, Apply<T10, T11> const& x1)
-{
-	return make_apply(x0, x1);
-}
-
-template <typename T0, typename T1>
-Apply<Apply<T0, T1>, Value> operator*(Apply<T0, T1> const& x0, Value const& x1)
-{
-	return make_apply(x0, x1);
-}
-
-template <typename T0, typename T1>
-Apply<Value, Apply<T0, T1> > operator*(Value const& x0, Apply<T0, T1> const& x1)
-{
-	return make_apply(x0, x1);
-}
 
 Code* box_function(Code code)
 {
@@ -166,9 +124,8 @@ Value make_combinator(Code code, void* data)
 //fail
 Value code_fail1(Value context, void* data, Value argument)
 {
-	Cell* f0 = (Cell*)data;
 	fprintf(stderr, "fail\n");
-	assert(0);
+	ASSERT(0);
 	exit(1);
 	return argument;
 }
@@ -183,18 +140,18 @@ Value fail = make_combinator(code_fail, 0);
 Value code_compose2(Value context, void* data, Value argument)
 {
 	Cell* fs = (Cell*)data;
-	assert(fs);
+	ASSERT(fs);
 	Cell* f0 = (Cell*)(fs->head);
-	assert(f0);
+	ASSERT(f0);
 	Cell* f1 = (Cell*)(fs->tail);
-	assert(f1);
+	ASSERT(f1);
 	return apply(context, Value(f0), apply(context, Value(f1), argument));
 }
 
 Value code_compose1(Value context, void* data, Value argument)
 {
 	Cell* f0 = (Cell*)data;
-	assert(f0);
+	ASSERT(f0);
 	return make_combinator(code_compose2, make_cell(f0, argument.cell));
 }
 
@@ -209,18 +166,18 @@ Value compose = make_combinator(code_compose, 0);
 Value code_flip2(Value context, void* data, Value argument)
 {
 	Cell* fs = (Cell*)data;
-	assert(fs);
+	ASSERT(fs);
 	Cell* f0 = (Cell*)(fs->head);
-	assert(f0);
+	ASSERT(f0);
 	Cell* f1 = (Cell*)(fs->tail);
-	assert(f1);
+	ASSERT(f1);
 	return apply(context, apply(context, Value(f0), argument), Value(f1));
 }
 
 Value code_flip1(Value context, void* data, Value argument)
 {
 	Cell* f0 = (Cell*)data;
-	assert(f0);
+	ASSERT(f0);
 	return make_combinator(code_flip2, make_cell(f0, argument.cell));
 }
 
@@ -235,13 +192,13 @@ Value flip = make_combinator(code_flip, 0);
 Value code_constant1(Value context, void* data, Value /*argument*/)
 {
 	Cell* f0 = (Cell*)data;
-	assert(f0);
+	ASSERT(f0);
 	return f0;
 }
 
 Value code_constant(Value context, void* /*data*/, Value argument)
 {
-	assert(argument.cell);
+	ASSERT(argument.cell);
 	return make_combinator(code_constant1, argument.cell);
 }
 
@@ -251,7 +208,7 @@ Value constant = make_combinator(code_constant, 0);
 Value code_duplicate1(Value context, void* data, Value argument)
 {
 	Cell* f0 = (Cell*)data;
-	assert(f0);
+	ASSERT(f0);
 	return apply(context, apply(context, Value(f0), argument), argument);
 }
 
@@ -262,10 +219,83 @@ Value code_duplicate(Value context, void* /*data*/, Value argument)
 
 Value duplicate = make_combinator(code_duplicate, 0);
 
-ASSERT(compose * flip * constant * fail == flip * (constant * fail));
+// DSL
+Value evaluate(Value context, Value v)
+{
+	return v;
+}
+
+template <typename T0, typename T1> Value evaluate(
+		Value context, Apply<T0, T1> const& application)
+{
+	return apply(context, application.function, application.argument);
+}
+
+template <typename T0, typename T1> Apply<T0, T1>::operator Value() const
+{
+	return evaluate(Value(0), *this);
+}
+
+template <typename T0, typename T1>
+Apply<T0, T1> make_apply(T0 const& x0, T1 const& x1)
+{
+	return Apply<T0, T1>(x0, x1);
+}
+
+//operator*
+Apply<Value, Value> operator*(Value const& x0, Value const& x1)
+{
+	return make_apply(x0, x1);
+}
+
+template <typename T00, typename T01, typename T10, typename T11>
+Apply<Apply<T00, T01>, Apply<T10, T11> > operator*(
+		Apply<T00, T01> const& x0, Apply<T10, T11> const& x1)
+{
+	return make_apply(x0, x1);
+}
+
+template <typename T0, typename T1>
+Apply<Apply<T0, T1>, Value> operator*(Apply<T0, T1> const& x0, Value const& x1)
+{
+	return make_apply(x0, x1);
+}
+
+template <typename T0, typename T1>
+Apply<Value, Apply<T0, T1> > operator*(Value const& x0, Apply<T0, T1> const& x1)
+{
+	return make_apply(x0, x1);
+}
+
+//operator+
+Apply<Apply<Value, Value>, Value> operator+(Value const& x0, Value const& x1)
+{
+	return compose * x0 * x1;
+}
+
+template <typename T00, typename T01, typename T10, typename T11>
+Apply<Apply<Value, Apply<T00, T01> >, Apply<T10, T11> > operator+(
+		Apply<T00, T01> const& x0, Apply<T10, T11> const& x1)
+{
+	return compose * x0 * x1;
+}
+
+template <typename T0, typename T1>
+Apply<Apply<Value, Apply<T0, T1> >, Value> operator+(Apply<T0, T1> const& x0, Value const& x1)
+{
+	return compose * x0 * x1;
+}
+
+template <typename T0, typename T1>
+Apply<Apply<Value, Value>, Apply<T0, T1> > operator+(Value const& x0, Apply<T0, T1> const& x1)
+{
+	return compose * x0 * x1;
+}
+
+ASSERT((flip + constant) * fail == flip * (constant * fail));
 ASSERT(compose == flip * constant * fail * compose);
 ASSERT(compose == constant * compose * fail);
-ASSERT(duplicate * compose * fail == compose * fail * fail);
+ASSERT(duplicate * compose * fail == fail + fail);
 
 //identity
 Value identity = duplicate * constant;
@@ -285,7 +315,7 @@ Value boolean(bool x)
 }
 
 //cons
-Value cons = compose * flip * (flip * identity);
+Value cons = flip + (flip * identity);
 ASSERT(identity * compose * fail == compose * fail);
 ASSERT(flip * identity * compose * constant == constant * compose);
 ASSERT(cons * fail * compose * true_ == fail);
@@ -300,9 +330,10 @@ Value cdr = flip * identity * false_;
 ASSERT(cdr * (cons * fail * compose) == compose);
 
 //symbol
-Value code_symbol(Value context, void* /*data*/, Value argument)
+Value code_symbol(Value /*context*/, void* /*data*/, Value /*argument*/)
 {
-	assert(0);
+	ASSERT(0);
+	return fail;
 }
 
 Value symbol(char const* str)
@@ -313,10 +344,64 @@ Value symbol(char const* str)
 //symbolp
 Value code_symbolp(Value context, void* /*data*/, Value argument)
 {
-	assert(argument.cell);
+	ASSERT(argument.cell);
 	Code* box = (Code*)argument.cell->head;
 	return boolean(*box == code_symbol);
 }
+
+//identical
+Value code_identical1(Value context, void* data, Value argument)
+{
+	Cell* f0 = (Cell*)data;
+	ASSERT(f0);
+	return boolean(f0 == argument.cell);
+}
+
+Value code_identical(Value context, void* /*data*/, Value argument)
+{
+	ASSERT(argument.cell);
+	return make_combinator(code_identical1, argument.cell);
+}
+
+Value identical = make_combinator(code_identical, 0);
+ASSERT(identical * fail * fail == true_);
+ASSERT(identical * fail * compose == false_);
+
+//code
+Value code_code(Value /*context*/, void* /*data*/, Value /*argument*/)
+{
+	ASSERT(0);
+	return fail;
+}
+
+Value make_code(Code* code)
+{
+	return make_combinator(code_code, code);
+}
+
+//codeof
+Value code_codeof(Value context, void* /*data*/, Value argument)
+{
+	ASSERT(argument.cell);
+	Code* code = (Code*)argument.cell->head;
+	ASSERT(code);
+	return make_code(code);
+}
+
+Value codeof = make_combinator(code_codeof, 0);
+ASSERT(codeof * fail == codeof * fail);
+ASSERT(codeof * compose != codeof * fail);
+
+//composep
+namespace detail
+{
+	Value compose_sample0 = fail + fail;
+	Value compose_code = codeof * compose_sample0;
+	Value compose_sample1 = true_ + false_;
+}
+Value composep = identical * detail::compose_code + codeof;
+ASSERT(false_ == composep * fail);
+ASSERT(true_ == composep * detail::compose_sample1);
 
 //lambda
 
